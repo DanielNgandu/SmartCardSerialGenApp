1cc5c9bc3b4834dcbf4e188b0d7bd3333b236f3f
Daniel Ng`andu <daniel_ngandu@outlook.com>
2020-09-27 17:41:48 +0200

    2nd commit

---
+++ app/Models/Post.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+
+class Post extends Model
+{
+    use CrudTrait;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'published_at',
+        'author_id',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'author_id' => 'integer',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'published_at',
+    ];
+
+
+
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
